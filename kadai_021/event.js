277a224e61e330e5cd03941f6279b1f33c6cea13
megumegumi <a.flourrose1223@icloud.com>
2025-03-01 22:36:04 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,7 @@
+const btnItem = document.getElementById('btn');
+const textItem = document.getElementById('text');
+btnItem.addEventListener('click', () => {
+  setTimeout(() => {
+   textItem.textContent = "ボタンをクリックしました";
+  }, 2000);
+});
